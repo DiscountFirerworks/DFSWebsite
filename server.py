8415c1c43a012c1c7171925cfe1dc57ce5c41c84
cforsythe <cforsythe97@gmail.com>
2017-07-19 19:59:25 -0700

    Page is now loading a portion of database as a sample

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import sqlite3
 from sqlite3 import Error
 app = Flask(__name__)
@@ -21,14 +21,20 @@ def hello():
 @app.route("/gettable/")
 def getTable():
     conn = create_connection()
+    limit = request.args.get('limit')
     with conn:
         cur = conn.cursor()
-        cur.execute('SELECT * FROM "beginning inventory"')
+        if limit != None:
+            cur.execute('SELECT * FROM "beginning inventory"' + limit)
+        else:
+            cur.execute('SELECT * FROM "beginning inventory"')
         rows = cur.fetchall()
         table = []
         for row in rows:
             table.append(tuple(row))
-        print(table)
-        return jsonify(table=table)
+        names = [description[0] for description in cur.description]
+        print(names)
+        return jsonify(table=table,
+                columnnames=names)
 
 app.run(debug=True, host="0.0.0.0", port=80)
